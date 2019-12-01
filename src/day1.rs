b1af9affd87f39ec090a0a33ab09300fda345dee
Andrii Sherman <watemerald@gmail.com>
2019-11-30 23:45:36 -0500

    Skeleton for 2019

---
+++ src/day1.rs
@@ -0,0 +1,17 @@
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc(day1, part1)]
+fn part1() {}
+
+#[aoc(day1, part1)]
+fn part2() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_day1_part1() {
+        unimplemented!();
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+use aoc_runner_derive::aoc_main;
+
+aoc_main! {lib = rust_advent_of_code_2019 }
